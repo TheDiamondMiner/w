b2919ce0ba78d3ff3e5ceb24ec26f0685b05487d
TheDiamondMiner <73134219+TheDiamondMiner@users.noreply.github.com>
2023-12-15 02:04:19 +0530

    Add files via upload

--- something.py
+++ something.py
@@ -1,83 +1,55 @@
-# Import necessary libraries
 import time
-import subprocess
-import Adafruit_GPIO.SPI as SPI
 import Adafruit_SSD1306
-from PIL import Image
-from PIL import ImageDraw
-from PIL import ImageFont
+from PIL import Image, ImageDraw, ImageFont
 
-# Raspberry Pi pin configuration:
-RST = None  # on the PiOLED this pin isn't used
-DC = 23
-SPI_PORT = 0
-SPI_DEVICE = 0
+# Create an SSD1306 OLED object.
+disp = Adafruit_SSD1306.SSD1306_128_32(rst=None)
 
-# Initialize the SSD1306 OLED display
-disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST)
-
-# Initialize library
+# Initialize the library.
 disp.begin()
 
-# Clear display
+# Clear the display.
 disp.clear()
 disp.display()
 
-# Constants for OLED display
-DISPLAY_WIDTH = 128
-DISPLAY_HEIGHT = 32
-CHAR_WIDTH = 8  # Each character is approximately 8 pixels wide in the default font
-MAX_CHARS_PER_LINE = DISPLAY_WIDTH // CHAR_WIDTH
+# Create a blank image for drawing.
+width = disp.width
+height = disp.height
+image = Image.new('1', (width, height))
 
-# Function to print text on the OLED display
-def print_text_on_display(display, text):
-    # Split the text into words
-    words = text.split()
+# Get a drawing object.
+draw = ImageDraw.Draw(image)
 
-    # Iterate through the words
-    while len(words) > 0:
-        # Clear the display
-        display.clear()
-        display.display()
-
-        # Create a blank image for drawing
-        image = Image.new('1', (DISPLAY_WIDTH, DISPLAY_HEIGHT))
-        draw = ImageDraw.Draw(image)
-
-        # Set initial variables
-        current_line = 0
-        current_char_count = 0
-
-        # Iterate through the words to display one line at a time
-        while len(words) > 0:
-            word = words.pop(0)
-            word_length = len(word)
-
-            if current_char_count + word_length + 1 <= MAX_CHARS_PER_LINE:
-                draw.text((current_char_count * CHAR_WIDTH, current_line * 8), word, font=font, fill=255)
-                current_char_count += word_length + 1
-            else:
-                current_line += 1
-                current_char_count = 0
-
-                if current_line >= DISPLAY_HEIGHT // 8:
-                    break
-
-                draw.text((current_char_count * CHAR_WIDTH, current_line * 8), word, font=font, fill=255)
-                current_char_count += word_length + 1
-
-        # Display the image on the OLED
-        display.image(image)
-        display.display()
-        time.sleep(2)  # Display each sequence for 2 seconds before moving to the next
-
-# Example stats file to get text input
-stats_text = """
-this is a big paragraph to test the oled logic, i hope this works as if it does not then i am done for. Thank you everyone that helped in making this project a success and i reach nationals. Steve kobs is such a nice guy.
-"""
-
-# Load default font
+# Load default font.
 font = ImageFont.load_default()
 
-# Call the function with the stats text
-print_text_on_display(disp, stats_text)
+def display_text(text):
+    # Draw a black filled box to clear the image.
+    draw.rectangle((0,0,width,height), outline=0, fill=0)
+
+    # Split the text into lines that fit the display width.
+    lines = []
+    line = []
+    line_width = 0
+    words = text.split(' ')
+    for word in words:
+        word_width, word_height = draw.textsize(' '.join(line + [word]), font=font)
+        if word_width > width:
+            # This line is full, store it and start a new one.
+            lines.append(' '.join(line))
+            line = [word]
+        else:
+            line.append(word)
+    # Store the last line.
+    lines.append(' '.join(line))
+
+    # Write each line to the display, scrolling up for each new line.
+    for line in lines:
+        draw.text((0, 0), line, font=font, fill=255)
+        disp.image(image)
+        disp.display()
+        time.sleep(0.5)
+        draw.rectangle((0,0,width,height), outline=0, fill=0)
+
+# Test the function with your text.
+display_text("This is a test of the OLED text display function.")
