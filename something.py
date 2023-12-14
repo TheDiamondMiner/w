fc5e706a65ee1714eeb26d9170c1a913777a28b3
TheDiamondMiner <73134219+TheDiamondMiner@users.noreply.github.com>
2023-12-14 18:15:56 +0530

    Add files via upload

--- something.py
+++ something.py
@@ -7,38 +7,6 @@ from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
 
-# Function to print text on the OLED display
-def print_text_on_display(display, text):
-    display.clear()
-    display.display()
-
-    words = text.split()
-
-    current_line = 0
-    current_char_count = 0
-
-    for word in words:
-        word_length = len(word)
-
-        if current_char_count + word_length + 1 <= MAX_CHARS_PER_LINE:
-            display.text((current_char_count * CHAR_WIDTH, current_line * 8), word, fill=255)
-            current_char_count += word_length + 1
-        else:
-            current_line += 1
-            current_char_count = 0
-
-            if current_line >= DISPLAY_HEIGHT // 8:
-                display.clear()
-                display.display()
-                current_line = 0
-                print_text_on_display(display, ' '.join(words[words.index(word):]))
-                break
-
-            display.text((current_char_count * CHAR_WIDTH, current_line * 8), word, fill=255)
-            current_char_count += word_length + 1
-
-        display.display()
-
 # Raspberry Pi pin configuration:
 RST = None  # on the PiOLED this pin isn't used
 DC = 23
@@ -60,6 +28,44 @@ DISPLAY_WIDTH = 128
 DISPLAY_HEIGHT = 32
 CHAR_WIDTH = 8  # Each character is approximately 8 pixels wide in the default font
 MAX_CHARS_PER_LINE = DISPLAY_WIDTH // CHAR_WIDTH
+
+# Function to print text on the OLED display
+def print_text_on_display(display, text):
+    # Clear image buffer
+    image = Image.new('1', (DISPLAY_WIDTH, DISPLAY_HEIGHT))
+    draw = ImageDraw.Draw(image)
+
+    words = text.split()
+
+    current_line = 0
+    current_char_count = 0
+
+    font = ImageFont.load_default()  # Load the default font
+
+    for word in words:
+        word_length = len(word)
+        
+        if current_char_count + word_length + 1 <= MAX_CHARS_PER_LINE:
+            draw.text((current_char_count * CHAR_WIDTH, current_line * 8), word, font=font, fill=255)
+            current_char_count += word_length + 1
+        else:
+            current_line += 1
+            current_char_count = 0
+
+            if current_line >= DISPLAY_HEIGHT // 8:
+                display.image(image)
+                display.display()
+                time.sleep(1)
+                current_line = 0
+                draw.rectangle((0, 0, DISPLAY_WIDTH, DISPLAY_HEIGHT), outline=0, fill=0)
+                print_text_on_display(display, ' '.join(words[words.index(word):]))
+                break
+
+            draw.text((current_char_count * CHAR_WIDTH, current_line * 8), word, font=font, fill=255)
+            current_char_count += word_length + 1
+
+    display.image(image)
+    display.display()
 
 # Example stats file to get text input
 stats_text = """
